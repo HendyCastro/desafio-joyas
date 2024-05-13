2286b1d689a36743ae8bdac1f0346c377615bf34
HendyCastro <hendy0702@gmail.com>
2024-05-13 15:26:02 -0400

    subiendo

---
+++ consultas.js
@@ -0,0 +1,52 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'naya',
+    host: 'localhost',
+    database: 'joyas',
+    password: 'naya1234',
+    allowExitOnIdle: true
+});
+
+const getData = async (limit, page, order_by) => {
+
+    let consultas = "";
+    if (order_by) {
+        const [columna, orden] = order_by.split("_");
+        consultas += ` ORDER BY ${columna} ${orden}`;
+    }
+    if (limit) {
+        consultas += ` LIMIT ${limit}`
+    }
+
+    if (page && limit) {
+        const offset = (page * limit) - limit
+        consultas += `OFFSET ${offset}`
+    }
+
+    const query = `SELECT * FROM inventario ${consultas};`
+    const { rows } = await pool.query(query)
+    const resumenData = rows.map(element => {
+        return {
+            nombre: element.nombre,
+            ruta: `joyas/joya/:${element.id}`
+        }
+    })
+    return resumenData
+}
+const getDetalle = async (id) => {
+    const query = "SELECT * FROM inventario WHERE id= $1;"
+    const values = [id]
+    const { rows } = await pool.query(query, values)
+    return rows
+};
+
+const ejecutarConsulta = async (query, values) => {
+    const { rows } = await pool.query(query, values)
+    return rows;
+};
+
+
+
+
+module.exports = { ejecutarConsulta, getData, getDetalle }
---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const { getData, getDetalle, ejecutarConsulta } = require("./consultas.js");
+
+
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+
+
+app.get("/joyas", async (req, res) => {
+
+    try {
+        const { limit, page, order_by } = req.query
+        const data = await getData(limit, page, order_by)
+        const totalJoyas = data.length;
+        const stockTotal = data.reduce((acumulador, valorActual) => acumulador + valorActual.stock, 0)
+        const estructuraHateoas = { totalJoyas, stockTotal, data }
+        res.send(estructuraHateoas);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/joyas/joya/:id", async (req, res) => {
+    try {
+        const { id } = req.params
+        const detalleData = await getDetalle(id)
+        console.log(detalleData)
+        res.send(detalleData)
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/joyas/filtros", async (req, res) => {
+    try {
+        const { precio_max, precio_min, categoria, metal } = req.query;
+        let filtros = [];
+        const values = [];
+        const agregarFiltro = (campo, comparador, valor) => {
+            values.push(valor);
+            const posicion = filtros.length + 1;
+            filtros.push(`${campo} ${comparador} $${posicion}`);
+        };
+        if (precio_max) {
+            agregarFiltro("precio", "<=", precio_max);
+        }
+
+        if (precio_min) {
+            agregarFiltro("precio", ">=", precio_min);
+        }
+
+        if (categoria) {
+            agregarFiltro("categoria", "=", categoria);
+        }
+
+        if (metal) {
+            agregarFiltro("metal", "=", metal);
+        }
+
+        const unionDeFiltros = filtros.join(" AND ");
+        const query = `SELECT * FROM inventario WHERE ${unionDeFiltros};`;
+        const rows = await ejecutarConsulta(query, values);
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
